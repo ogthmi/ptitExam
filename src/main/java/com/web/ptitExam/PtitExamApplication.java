a564648e91cd8e4cc9023373fadf216c46f2e29d
ogthmi <ongminh.cbg@gmail.com>
2024-09-29 00:12:51 +0700

    SpringBoot Initializr

---
+++ src/main/java/com/web/ptitExam/PtitExamApplication.java
@@ -0,0 +1,13 @@
+package com.web.ptitExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtitExamApplication.class, args);
+	}
+
+}
